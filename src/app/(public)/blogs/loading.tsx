38973ac9911350f35fbe9bd0dc98917adb9dc19a
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-09-22 20:39:32 +0600

    nextjs intro

---
+++ src/app/(public)/blogs/loading.tsx
@@ -0,0 +1,11 @@
+import Loading from '@/components/ui/Loading';
+
+const BlogsLoadingPage = () => {
+    return (
+        <div className='py-20'>
+            <Loading />
+        </div>
+    );
+};
+
+export default BlogsLoadingPage;
